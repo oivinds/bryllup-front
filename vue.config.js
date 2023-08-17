9376fa4da93681d620241b05c46cc4bc7e05037f
Oivind Svenkerud <oivinds@users.noreply.github.com>
2023-08-17 22:49:51 +0200

    depl

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	transpileDependencies: ["vuetify"],
-
+	publicPath: process.env.NODE_ENV === "production" ? "/bryllup-front/" : "/",
 	pluginOptions: {
 		moment: {
 			locales: ["nb"],
